1cf03fb2d73e971b5ec98123e6e971f2aa4c4c1e
Saurabhh Kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-05-28 20:25:37 +0530

    Solution Leetcode and Explaination Every single line

--- Contain Duplicate 2/Solution.java
+++ Contain Duplicate 2/Solution.java
@@ -14,3 +14,21 @@ public class Solution {
         return false;
     }
 }
+
+// This solution has a time complexity of O(n) because it only needs to iterate through the array once, and a space complexity of O(n) because in the worst case, it needs to store all elements of the array in the map.
+
+
+// import java.util.HashMap;
+// import java.util.Map;
+// public class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+//                 return true;
+//             }
+//             map.put(nums[i], i);
+//         }
+//         return false;
+//     }
+// }

74a019a9dc044cd67d84da521d60f8a2a9c83d75
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-05-29 20:18:32 +0530

    Solution Leetcode and Explaination Every single line

---
+++ Two Sum/Solution 1 With Explaination/Solution1.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+            numToIndex.put(nums[i], i);
+        }
+        
+        // This line will never be reached if the input is valid
+        throw new RuntimeException("No solution found");
+    }
+}
+
+
+
+
+/* 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1] 
+
+*/

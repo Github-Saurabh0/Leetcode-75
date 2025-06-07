ccfc6205e65206591081d58df30b49d428b5f171
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-07 12:06:49 +0530

    Solution Leetcode and Explaination Every single line

---
+++ 36. Evaluate Reverse Polish Notation/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();  // Second operand
+                int a = stack.pop();  // First operand
+                stack.push(applyOp(a, b, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int applyOp(int a, int b, String op) {
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            default:  return a / b;  // Division
+        }
+    }
+}
